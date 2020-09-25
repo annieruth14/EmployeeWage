dcdc0d7f153dfd1d4907e75db6392d72dc87850e
annie <ruthannie3@gmail.com>
2020-09-25 14:57:55 +0530

    [annie]Added: class for computing total wage for each company

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,14 +1,25 @@
 public class EmployeeWage{
-	// Constants
 	public static final int IS_PART_TIME = 2;
 	public static final int IS_FULL_TIME = 1;
-	
 
-	public static int calEmpWage(String company, int empRate, int no_of_days, int max_hrs){
+	private final String company;
+	private final int empRate;
+	private final int no_of_days;
+	private final int max_hrs;
+	private int totalEmpWage;
+
+	public EmployeeWage (String company, int empRate, int no_of_days, int max_hrs){
+		this.company = company;
+		this.empRate = empRate;
+		this.no_of_days = no_of_days;
+		this.max_hrs = max_hrs; 	
+	}
+
+	public void calEmpWage(){
 		// variables
 		int empHrs = 0;
 		int empWage = 0;
-		int totalEmpWage = 0;
+		
 		int totalDays = 0; 
 		int totalHrs = 0;
 
@@ -34,17 +45,34 @@ public class EmployeeWage{
 			System.out.println("Day: "+ totalDays + " Emp hour: "+ totalHrs);
 			
 		}
-		System.out.println("Total Emp Wage for Company "+company + " is : " +totalEmpWage);
-		return totalEmpWage;
-	}
-
-
-	public static void main (String[] args){
-		calEmpWage("Capgemini",20,2,10); 
-		calEmpWage("Reliance",10,4,20); 
 		
 	}
+	
+	public static void main (String[] args){
+		EmployeeWage Capg = new EmployeeWage("Capgemini",20,3,8);
+		EmployeeWage Rel = new EmployeeWage("Reliance",30,2,11);
+		Capg.calEmpWage();
+		Rel.calEmpWage();
+		System.out.println("Total Emp Wage for Company "+Capg.company + " is : " +Capg.totalEmpWage);
+		System.out.println("Total Emp Wage for Company "+Rel.company + " is : " +Rel.totalEmpWage);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
