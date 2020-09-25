e4c78d3f51b99a05ddabdd40f8c85eda88dc7e89
annie <ruthannie3@gmail.com>
2020-09-25 14:53:42 +0530

    [annie]Added: class for computing total wage for each company

---
+++ EmployeeWageBuilder.java
@@ -0,0 +1,79 @@
+public class EmployeeWageBuilder{
+	public static final int IS_PART_TIME = 2;
+	public static final int IS_FULL_TIME = 1;
+
+	private final String company;
+	private final int empRate;
+	private final int no_of_days;
+	private final int max_hrs;
+	private int totalEmpWage;
+
+	public EmployeeWageBuilder (String company, int empRate, int no_of_days, int max_hrs){
+		this.company = company;
+		this.empRate = empRate;
+		this.no_of_days = no_of_days;
+		this.max_hrs = max_hrs; 	
+	}
+
+	public void calEmpWage(){
+		// variables
+		int empHrs = 0;
+		int empWage = 0;
+		
+		int totalDays = 0; 
+		int totalHrs = 0;
+
+		// Computation
+		while(totalDays < no_of_days && totalHrs <= max_hrs){
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+				
+			switch (empCheck) {
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				default:
+					empHrs = 0;
+					
+			}
+			empWage = empHrs * empRate ;			
+			totalEmpWage = totalEmpWage + empWage; 
+			totalDays++;
+			totalHrs = totalHrs + empHrs;
+			System.out.println("Day: "+ totalDays + " Emp hour: "+ totalHrs);
+			
+		}
+		
+	}
+	
+	public static void main (String[] args){
+		EmployeeWageBuilder Capg = new EmployeeWageBuilder("Capgemini",20,3,8);
+		EmployeeWageBuilder Rel = new EmployeeWageBuilder("Reliance",30,2,11);
+		Capg.calEmpWage();
+		Rel.calEmpWage();
+		System.out.println("Total Emp Wage for Company "+Capg.company + " is : " +Capg.totalEmpWage);
+		System.out.println("Total Emp Wage for Company "+Rel.company + " is : " +Rel.totalEmpWage);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
