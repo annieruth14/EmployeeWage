ab3c4567c6a481fe9d76596876b6600a657f8e94
annie <ruthannie3@gmail.com>
2020-09-26 18:08:15 +0530

    [Annie]Refactored: Changed array to Arraylist

--- EmpWageArray.java
+++ EmpWageArray.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class EmpWageArray implements CompanyEmpWage{
 	public static final int IS_PART_TIME = 2;
 	public static final int IS_FULL_TIME = 1;
@@ -7,7 +9,10 @@ public class EmpWageArray implements CompanyEmpWage{
 	public int max_hrs;
 	public int totalEmpWage;
 
-	
+	public ArrayList<EmpWageArray> listOfComp = new ArrayList<EmpWageArray>();
+
+	public EmpWageArray(){
+	}
 	
 	public EmpWageArray (String company, int empRate, int no_of_days, int max_hrs){
 		this.company = company;
@@ -17,23 +22,16 @@ public class EmpWageArray implements CompanyEmpWage{
 	}
 
 
-	private int numOfCompany = 0;	//counter of companies
-	private EmpWageArray[] empWageArray;	//array of this class
-
-	public EmpWageArray(){	//assigning linit for array
-		empWageArray = new EmpWageArray[5];	
-	} 
-	
-	private void addCompanyEmpWage(String company, int empRate, int no_of_days, int max_hrs){	//adds company in array and increases the counter
-		empWageArray[numOfCompany] = new EmpWageArray(company, empRate, no_of_days, max_hrs);
-		numOfCompany++;
+	private void addCompanyEmpWage(String company, int empRate, int no_of_days, int max_hrs){	
+		EmpWageArray comp = new EmpWageArray(company, empRate, no_of_days, max_hrs);
+		listOfComp.add(comp);
 		
 	}
 	
 	private void calEmpWage(){
-		for(int i=0; i<numOfCompany; i++){
-			System.out.println("Company : " + empWageArray[i].company);
-			empWageArray[i].calTotalEmpWage(this.calEmpWage(empWageArray[i]));
+		for(EmpWageArray e : listOfComp){
+			System.out.println("Company : " + e.company);
+			e.calTotalEmpWage(this.calEmpWage(e));
 		}
 	}
 
@@ -73,7 +71,7 @@ public class EmpWageArray implements CompanyEmpWage{
 			System.out.println("Day: "+ totalDays + " Emp hour: "+ empHrs);
 			
 		}
-		System.out.println(totalHrs*empWageArray.empRate);
+		
 		return totalHrs*empWageArray.empRate ;
 	}
 
