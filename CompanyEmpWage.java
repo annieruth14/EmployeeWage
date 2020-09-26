3e48b610f8eb1ffd02795f258489661e188ff98b
annie <ruthannie3@gmail.com>
2020-09-26 14:43:23 +0530

    [Annie]Refactored: Manage employee wage of multiple companies using Interface approach

--- CompanyEmpWage.java
+++ CompanyEmpWage.java
@@ -1,19 +1,4 @@
-public class CompanyEmpWage{
-
-	public final String company;
-	public final int empRate;
-	public final int no_of_days;
-	public final int max_hrs;
-	public int totalEmpWage;
-	
-	public CompanyEmpWage (String company, int empRate, int no_of_days, int max_hrs){
-		this.company = company;
-		this.empRate = empRate;
-		this.no_of_days = no_of_days;
-		this.max_hrs = max_hrs; 	
-	}
-
-	public void setTotalEmpWage(int totalEmpWage){
-		this.totalEmpWage = totalEmpWage;
-	}
+public interface CompanyEmpWage{
+	int n=0;
+	public void calTotalEmpWage(int n);
 }
--- EmpWageArray.java
+++ EmpWageArray.java
@@ -1,37 +1,60 @@
-public class EmpWageArray{
+public class EmpWageArray implements CompanyEmpWage{
 	public static final int IS_PART_TIME = 2;
 	public static final int IS_FULL_TIME = 1;
-	
-	private int numOfCompany = 0;
-	private CompanyEmpWage[] companyEmpWageArray;
+	public String company;
+	public int empRate;
+	public int no_of_days;
+	public int max_hrs;
+	public int totalEmpWage;
 
-	public EmpWageArray(){
-		companyEmpWageArray = new CompanyEmpWage[5];	
+	
+	
+	public EmpWageArray (String company, int empRate, int no_of_days, int max_hrs){
+		this.company = company;
+		this.empRate = empRate;
+		this.no_of_days = no_of_days;
+		this.max_hrs = max_hrs; 	
+	}
+
+
+	private int numOfCompany = 0;	//counter of companies
+	private EmpWageArray[] empWageArray;	//array of this class
+
+	public EmpWageArray(){	//assigning linit for array
+		empWageArray = new EmpWageArray[5];	
 	} 
 	
-	private void addCompanyEmpWage(String company, int empRate, int no_of_days, int max_hrs){
-		companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, empRate, no_of_days, max_hrs);
+	private void addCompanyEmpWage(String company, int empRate, int no_of_days, int max_hrs){	//adds company in array and increases the counter
+		empWageArray[numOfCompany] = new EmpWageArray(company, empRate, no_of_days, max_hrs);
 		numOfCompany++;
+		
 	}
 	
 	private void calEmpWage(){
 		for(int i=0; i<numOfCompany; i++){
-			companyEmpWageArray[i].setTotalEmpWage(this.calEmpWage(companyEmpWageArray[i]));
+			System.out.println("Company : " + empWageArray[i].company);
+			empWageArray[i].calTotalEmpWage(this.calEmpWage(empWageArray[i]));
 		}
 	}
 
-	private int calEmpWage(CompanyEmpWage companyEmpWage){
+	public void calTotalEmpWage(int totalEmpWage){
+		
+		this.totalEmpWage = totalEmpWage;
+		System.out.println("The total Employee wage = " + totalEmpWage);
+	}
+
+	private int calEmpWage(EmpWageArray empWageArray){
 		// variables
 		int empHrs = 0;
 		int empWage = 0;
 		
 		int totalDays = 0; 
 		int totalHrs = 0;
-
+		
 		// Computation
-		while(totalDays < companyEmpWage.no_of_days && totalHrs <= companyEmpWage.max_hrs){
+		while(totalDays < empWageArray.no_of_days && totalHrs <= empWageArray.max_hrs){
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-				
+			
 			switch (empCheck) {
 				case IS_FULL_TIME:
 					empHrs = 8;
@@ -43,19 +66,21 @@ public class EmpWageArray{
 					empHrs = 0;
 					
 			}
-			empWage = empHrs * companyEmpWage.empRate ;			
+			empWage = empHrs * empRate ;			
 			
 			totalDays++;
 			totalHrs = totalHrs + empHrs;
 			System.out.println("Day: "+ totalDays + " Emp hour: "+ empHrs);
 			
 		}
-		return totalHrs;
+		System.out.println(totalHrs*empWageArray.empRate);
+		return totalHrs*empWageArray.empRate ;
 	}
 
 	public static void main (String[] args){
 		EmpWageArray emparray = new EmpWageArray();
 		emparray.addCompanyEmpWage("Capgemini",20,3,8);
+		
 		emparray.addCompanyEmpWage("Reliance",30,5,11);	
 		emparray.calEmpWage();
 	}
